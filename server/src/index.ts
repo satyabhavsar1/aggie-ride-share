3334fb31112a1f5ac8160ff64b06a1ccd0bcbdd2
satyabhavsar1 <satyabhavsar@tamu.edu>
2025-02-12 17:55:43 -0600

    initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
