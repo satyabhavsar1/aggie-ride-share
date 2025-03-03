1997c02e5eefd010aa916f8e59bf40c3af62aaca
satyabhavsar1 <satyabhavsar@tamu.edu>
2025-03-03 14:48:32 -0800

    Fix db connection error

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,6 +6,7 @@ const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5001/";
 
 export default function Home() {
   const [message, setMessage] = useState("");
+  const [data, setData] = useState("");
 
   useEffect(() => {
     console.log(API_BASE_URL);
@@ -14,10 +15,17 @@ export default function Home() {
       .catch(error => console.error(error));
   }, []);
 
+  useEffect(() => {
+    axios.get(`${API_BASE_URL}/api/test-db`)
+      .then(response => setData(response.data))
+      .catch(error => console.error("API Test DB error:", error));
+  }, []);
+  
   return (
     <div>
       <h1>React + Express + TypeScript</h1>
       <p>Message from backend is: {message}</p>
+      <p> {data}</p>
     </div>
   );
 }
