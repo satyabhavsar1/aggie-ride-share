92e14ec30369cce55f17c3ad26be639d8b4e2f18
satyabhavsar1 <satyabhavsar@tamu.edu>
2025-04-01 11:23:29 -0500

    Update user table name

--- server/src/migrations/1743104312647-UpdateUserMigration.ts
+++ server/src/migrations/1743104312647-UpdateUserMigration.ts
@@ -4,11 +4,11 @@ export class UpdateUserMigration1743104312647 implements MigrationInterface {
     name = 'UpdateUserMigration1743104312647'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "resetToken" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "resetToken" DROP NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "resetToken" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "resetToken" SET NOT NULL`);
     }
 
 }
--- server/src/migrations/AddTimeStampToRideMigration1743096842028.ts
+++ server/src/migrations/AddTimeStampToRideMigration1743096842028.ts
@@ -4,7 +4,7 @@ export class AddTimeStampToRideMigration1743096842028 implements MigrationInterf
     name = 'AddTimeStampToRideMigration1743096842028'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "passwordHash" character varying NOT NULL, "email" character varying NOT NULL, "registrationTime" TIMESTAMP NOT NULL DEFAULT now(), "resetToken" character varying NOT NULL, "tokenExpirationDate" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "passwordHash" character varying NOT NULL, "email" character varying NOT NULL, "registrationTime" TIMESTAMP NOT NULL DEFAULT now(), "resetToken" character varying NOT NULL, "tokenExpirationDate" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373761" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "ride_users_user" ("rideId" integer NOT NULL, "userId" integer NOT NULL, CONSTRAINT "PK_f4043e5196ac1922ecce4bfe2a2" PRIMARY KEY ("rideId", "userId"))`);
         await queryRunner.query(`CREATE INDEX "IDX_b39ce007c3f6757f5772c4e980" ON "ride_users_user" ("rideId") `);
         await queryRunner.query(`CREATE INDEX "IDX_dd7786a5287a74e84650b5deb7" ON "ride_users_user" ("userId") `);
@@ -12,7 +12,7 @@ export class AddTimeStampToRideMigration1743096842028 implements MigrationInterf
         await queryRunner.query(`ALTER TABLE "ride" ADD "created_time" TIMESTAMP NOT NULL DEFAULT now()`);
         await queryRunner.query(`ALTER TABLE "ride" ADD "updated_time" TIMESTAMP NOT NULL DEFAULT now()`);
         await queryRunner.query(`ALTER TABLE "ride_users_user" ADD CONSTRAINT "FK_b39ce007c3f6757f5772c4e980f" FOREIGN KEY ("rideId") REFERENCES "ride"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
-        await queryRunner.query(`ALTER TABLE "ride_users_user" ADD CONSTRAINT "FK_dd7786a5287a74e84650b5deb7b" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "ride_users_user" ADD CONSTRAINT "FK_dd7786a5287a74e84650b5deb7b" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -24,7 +24,7 @@ export class AddTimeStampToRideMigration1743096842028 implements MigrationInterf
         await queryRunner.query(`DROP INDEX "public"."IDX_dd7786a5287a74e84650b5deb7"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_b39ce007c3f6757f5772c4e980"`);
         await queryRunner.query(`DROP TABLE "ride_users_user"`);
-        await queryRunner.query(`DROP TABLE "user"`);
+        await queryRunner.query(`DROP TABLE "users"`);
     }
 
 }
